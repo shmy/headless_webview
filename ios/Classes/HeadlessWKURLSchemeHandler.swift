49a8da04d0e9793cc938c99112fbc6c134de7e59
chao.wang1 <chao.wang1@thoughtworks.com>
2022-02-16 20:56:22 +0800

    add try catch

--- ios/Classes/HeadlessWKURLSchemeHandler.swift
+++ ios/Classes/HeadlessWKURLSchemeHandler.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 import WebKit
+import SwiftTryCatch
 
 class HeadlessWKURLSchemeHandler: NSObject, WKURLSchemeHandler {
     public let channel: FlutterMethodChannel
     public let id: Int
     public var methodName = "intercepted"
     private var tasks: [URLSessionTask] = []
-    private var holdUrlSchemeTasks: [String: Bool] = [:]
     init(channel: FlutterMethodChannel, id: Int) {
         self.channel = channel
         self.id = id
@@ -23,21 +23,15 @@ class HeadlessWKURLSchemeHandler: NSObject, WKURLSchemeHandler {
     }
     public func cancelAll() {
         tasks.forEach { task in
-            // https://juejin.cn/post/6844903954212454413
-            if let isValid = self.holdUrlSchemeTasks[task.description] {
-                if (isValid && task.state == .running) {
-                    task.cancel()
-                }
+            if (task.state == .running) {
+                task.cancel()
             }
-           
         }
         tasks.removeAll()
     }
     
     @available(iOS 11.0, *)
-    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
-        holdUrlSchemeTasks[urlSchemeTask.description] = false
-    }
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {}
     @available(iOS 11.0, *)
 
     func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
@@ -45,21 +39,27 @@ class HeadlessWKURLSchemeHandler: NSObject, WKURLSchemeHandler {
        guard let url = urlSchemeTask.request.url else {
            return
        }
-       holdUrlSchemeTasks[urlSchemeTask.description] = true
        let task = URLSession.shared.dataTask(with: urlSchemeTask.request) { (data, response, error) in
            var arguments = [String: Any]()
            arguments["id"] = self.id
            arguments["url"] = url.absoluteString
-           if (error == nil) {
-               arguments["mimeType"] = response!.mimeType
-               self.channel.invokeMethod(self.methodName, arguments: arguments)
-               urlSchemeTask.didReceive(response!)
-               urlSchemeTask.didReceive(data!)
-               urlSchemeTask.didFinish()
-           } else {
-               self.channel.invokeMethod(self.methodName, arguments: arguments)
-               urlSchemeTask.didFailWithError(error!)
-           }
+           
+           SwiftTryCatch.try({
+                   if (error == nil) {
+                       arguments["mimeType"] = response!.mimeType
+                       self.channel.invokeMethod(self.methodName, arguments: arguments)
+                       urlSchemeTask.didReceive(response!)
+                       urlSchemeTask.didReceive(data!)
+                       urlSchemeTask.didFinish()
+                   } else {
+                       self.channel.invokeMethod(self.methodName, arguments: arguments)
+                       urlSchemeTask.didFailWithError(error!)
+                   }
+                }, catch: { (error) in
+                    print("\(String(describing: error?.description))")
+                }, finally: {
+                    // close resources
+           })
            
         }
         task.resume()
